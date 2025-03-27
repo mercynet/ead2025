7a42fe8b3a2cef969e0d601a38de345b6d3d08d8
Paulo Silva <esmeriu@gmail.com>
2025-03-27 21:19:07 +0000

    Add API routes for countries and courses, update app configuration, and include Blueprint package

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::apiResource('countries', App\Http\Controllers\Api\CountryController::class);
+
+Route::apiResource('courses', App\Http\Controllers\Api\CourseController::class);
+
+
+Route::apiResource('countries', App\Http\Controllers\Api\CountryController::class);
+
+Route::apiResource('courses', App\Http\Controllers\Api\CourseController::class);
+
+
+Route::apiResource('countries', App\Http\Controllers\Api\CountryController::class);
+
+Route::apiResource('courses', App\Http\Controllers\Api\CourseController::class);
